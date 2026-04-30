b47e848b34da2b83445a2fca717c78d2427a2117
xycdd00 <11654259@qq.com>
2026-05-01 02:07:21 +0800

    修复端口配置

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // 重要：用于解析JSON请求体
 app.use(express.json());
